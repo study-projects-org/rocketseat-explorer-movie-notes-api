ef3921872a3d68986c21599dc324cf4d8f3d9a18
Ronaldo Nunes <projetoftnunes@gmail.com>
2024-05-15 12:58:45 -0300

    feat: Add movie notes routes and controller

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { usersRoutes } from "./users.routes.js";
+import { movieNotesRoutes } from "./movie-notes.routes.js";
 
 const routes = Router();
 
 routes.use('/users', usersRoutes);
+routes.use('/movie-notes', movieNotesRoutes);
 
 export { routes };
