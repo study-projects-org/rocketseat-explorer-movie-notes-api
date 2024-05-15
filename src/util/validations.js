d83b6fb2ca71d7bdf4c1015e58f5cd6176569ac0
Ronaldo Nunes <projetoftnunes@gmail.com>
2024-05-15 12:33:40 -0300

    refactor: validation utility class for email, password, name, and rating

--- src/util/validations.js
+++ src/util/validations.js
@@ -1,19 +1,45 @@
 export class Validation {
 
   static email(email) {
+    if (!value) {
+      return { valid: false, error: 'Email not informed' };
+    }
     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(String(email).toLowerCase());
+    const validEmail = regex.test(String(email).toLowerCase());
+
+    if (!validEmail) {
+      return { valid: false, error: 'Invalid email format' };
+    }
+
+    return { valid: true };
   }
 
-  static password(password) {
-    return password && password.length >= 6;
+  static text({ value, minLength = 3, propName = 'field' }) {
+    if (!value) {
+      return { valid: false, error: `The ${propName} not informed` };
+    } else if (value.length < minLength) {
+      return { valid: false, error: `The ${propName} must contain at least ${minLength} characters` };
+    }
+
+    return { valid: true };
   }
 
-  static name(name) {
-    return name && name.length >= 3;
-  }
-
-  static rating(value){
-    return value && value >= 0 && value <= 5;
+  static rating(value) {
+    if (!value) {
+      return { valid: false, error: 'Rating not informed' };
+    }
+  
+    const isValidNumber = /^\d+$/.test(value);
+    const isBetween0And5 = value >= 0 && value <= 5;
+  
+    if (!isValidNumber) {
+      return { valid: false, error: 'The rating value must be an integer' };
+    }
+  
+    if (!isBetween0And5) {
+      return { valid: false, error: 'The ranting value must be between 0 and 5' };
+    }
+  
+    return { valid: true };
   }
 }
